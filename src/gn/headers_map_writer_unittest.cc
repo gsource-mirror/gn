2d43313f46a1bbdd3d8809742496cc1e622122d8
Matt Stark <msta@google.com>
2026-04-15 17:16:56 +1000

    Create a mapping from header files to labels that define them.
    The format is:
    ```
    bar.h //:bar //:bar
    foo.h //:foo
    ```
    
    The purpose of this is to allow a custom clang plugin to rewrite error
    messages from:
    foo.h:1:10: module "//:foo_Private" does not depend on a module exporting 'path/to/bar.h'
    #include "path/to/bar.h"
              ^
    
    to:
    path/to/foo.h:1:10: error: //foo:foo does not directly depend on //path/to:bar, which contains 'path/to/bar.h' in its public_deps
    #include "path/to/bar.h"
             ^
    path/to/foo.h:1:10: note: Consider adding deps = [ ":bar" ] to foo in path/to/BUILD.gn
    
    Note that this clang plugin will be multimodal.
    * When running locally, it will print the above message
    * When running on RBE, it will print an error message requesting you to
      run a script to print suggestions (since it won't have access to
      headers.txt in the remote action).
    
    Bug: b:500845363
    Change-Id: Ie8ff5563163db00ac5a630c988ebd6356a6a6964

---
+++ src/gn/headers_map_writer_unittest.cc
@@ -0,0 +1,45 @@
+// Copyright 2026 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gn/headers_map_writer.h"
+
+#include <map>
+#include <string_view>
+#include <vector>
+
+#include "gn/label.h"
+#include "gn/source_dir.h"
+#include "util/test/test.h"
+
+TEST(HeadersMapWriterTest, GenerateFiles) {
+  Label default_toolchain(SourceDir("//toolchain/"), "default");
+  Label second_toolchain(SourceDir("//toolchain/"), "second");
+  Label third_toolchain(SourceDir("//toolchain/"), "third");
+
+  auto make_label = [](auto name, const Label& toolchain) {
+    return Label(SourceDir("//"), name, toolchain.dir(), toolchain.name());
+  };
+
+  auto a = make_label("a", default_toolchain);
+  auto a_second = make_label("a", second_toolchain);
+  auto a_third = make_label("a", third_toolchain);
+  auto b = make_label("b", default_toolchain);
+
+  std::map<std::string_view, std::vector<const Label*>> header_to_targets = {
+      {"two_labels.h", {&a, &b}},
+      {"default_included.h", {&a, &a_second, &a_third}},
+      {"default_not_included.h", {&a_second, &a_third}},
+  };
+
+  auto got =
+      HeadersMapWriter::GenerateFiles(default_toolchain, header_to_targets)
+          .str();
+
+  std::string expected = R"##(default_included.h //:a
+default_not_included.h //:a
+two_labels.h //:a //:b
+)##";
+
+  EXPECT_EQ(got, expected) << got << "\n" << expected;
+}

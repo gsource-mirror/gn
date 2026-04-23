51dba0512045efa2514327ec3f0585deee2ecf53
Matt Stark <msta@google.com>
2026-04-23 14:49:09 +1000

    Run formatter and update reference in CI
    When --diff is provided, they output nonzero statuses when changes are
    required. This makes them suitable to run in CI
    
    Change-Id: Iceda9895beae955e13059e74739810126a6a6964

--- tools/run_formatter.sh
+++ tools/run_formatter.sh
@@ -1,12 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 cd $(dirname $(dirname $0))
 
-ensure_file=$(mktemp)
+check=false
+if [ "$1" = "--diff" ]; then
+  check=true
+fi
 
-# https://chrome-infra-packages.appspot.com/p/fuchsia/third_party/clang
-echo 'fuchsia/third_party/clang/${platform} integration' > $ensure_file
-cipd ensure -ensure-file $ensure_file -root clang
+if [ -z "${CLANG_FORMAT:-}" ]; then
+  ensure_file=$(mktemp)
+  # https://chrome-infra-packages.appspot.com/p/fuchsia/third_party/clang
+  echo 'fuchsia/third_party/clang/${platform} integration' > $ensure_file
+  cipd ensure -ensure-file $ensure_file -root clang
+  CLANG_FORMAT="./clang/bin/clang-format"
+fi
 
-git ls-files | egrep '\.(h|cc)$' | fgrep -v 'third_party' |\
-    xargs ./clang/bin/clang-format -i
+has_diff=0
+for f in $(git ls-files | egrep '\.(h|cc)$' | fgrep -v 'third_party'); do
+    if "${check}"; then
+        diff -u "$f" <("$CLANG_FORMAT" "$f") || has_diff=1
+    else
+        "$CLANG_FORMAT" -i "$f"
+    fi
+done
+exit $has_diff

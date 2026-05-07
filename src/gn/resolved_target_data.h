d366ee9d1203b7bf92c56979eb6bb7c1dfbe3b3d
Takuto Ikuta <tikuta@google.com>
2026-05-07 16:17:46 +0900

    Optimize OrderOnlyDeps computation using ResolvedTargetData caching
    This is a follow-up to https://gn-review.googlesource.com/c/gn/+/22400
    to address unresolved review comments.
    
    - Add GetOrderOnlyDeps() to ResolvedTargetData to compute and cache order-only dependencies of targets in a bottom-up manner.
    - Generalize this by making group() targets export the order-only dependencies of their direct dependents.
    - Refactor NinjaBinaryTargetWriter::GetOrderOnlyDepsFromNonLinkableDeps to use this cached data and simplify duplicate elimination using UniqueVector<OutputFile>.
    
    Bug: 502431091
    Change-Id: I6a2486a00bc9efe98629799601e8c149113bbdcb

--- src/gn/resolved_target_data.h
+++ src/gn/resolved_target_data.h
@@ -18,6 +18,7 @@
 #include "gn/target.h"
 #include "gn/target_public_pair.h"
 #include "gn/unique_vector.h"
+#include "gn/output_file.h"
 
 // A class used to compute target-specific data by collecting information
 // from its tree of dependencies.
@@ -145,6 +146,11 @@ class ResolvedTargetData {
     return info->swift_values->modules;
   }
 
+  // Retrieves an ordered list of all order-only dependency outputs for this target.
+  const std::vector<OutputFile>& GetOrderOnlyDeps(const Target* target) const {
+    return GetTargetOrderOnlyDeps(target)->order_only_deps;
+  }
+
  private:
   // The information associated with a given Target pointer.
   struct TargetInfo {
@@ -167,6 +173,7 @@ class ResolvedTargetData {
     std::atomic<bool> has_module_deps_information = false;
     std::atomic<bool> has_rust_libs = false;
     std::atomic<bool> has_swift_values = false;
+    std::atomic<bool> has_order_only_deps = false;
 
     // Only valid if |has_lib_info| is true.
     std::vector<SourceDir> lib_dirs;
@@ -205,6 +212,9 @@ class ResolvedTargetData {
             public_modules(std::move(public_modules)) {}
     };
     std::unique_ptr<SwiftValues> swift_values;
+
+    // Only valid if |has_order_only_deps| is true.
+    std::vector<OutputFile> order_only_deps;
   };
 
   // Retrieve TargetInfo value associated with |target|. Create
@@ -296,6 +306,18 @@ class ResolvedTargetData {
     return info;
   }
 
+  const TargetInfo* GetTargetOrderOnlyDeps(const Target* target) const {
+    TargetInfo* info = GetTargetInfo(target);
+    if (!info->has_order_only_deps.load(std::memory_order_acquire)) {
+      std::lock_guard<std::mutex> lock(info->mutex);
+      if (!info->has_order_only_deps.load(std::memory_order_relaxed)) {
+        ComputeOrderOnlyDeps(info);
+        info->has_order_only_deps.store(true, std::memory_order_release);
+      }
+    }
+    return info;
+  }
+
   // Compute the portion of TargetInfo guarded by one of the |has_xxx|
   // booleans. This performs recursive and expensive computations and
   // should only be called once per TargetInfo instance.
@@ -306,6 +328,7 @@ class ResolvedTargetData {
   void ComputeModuleDepsInformation(TargetInfo* info) const;
   void ComputeRustLibs(TargetInfo* info) const;
   void ComputeSwiftValues(TargetInfo* info) const;
+  void ComputeOrderOnlyDeps(TargetInfo* info) const;
 
   // Helper function used by ComputeInheritedLibs().
   void ComputeInheritedLibsFor(

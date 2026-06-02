76d50fed6508273923e3c71ec41f514a2e83d565
David 'Digit' Turner <digit@google.com>
2026-06-02 18:25:16 +0200

    Introduce --format-width option (and format_width .gn variable).
    The change in [1] considerably changed the output
    of `gn format` which makes a new version of GN unrollable
    to the Fuchsia tree (as this breaks many shac tests).
    
    Since fixing all build files in Fuchsia is going to take
    too long, this introduces a new .gn variable named 'format_width'
    and associated command-line flag --format-width to override
    the default to a new value.
    
    By setting `format_width` to 100, the changes to the Fuchsia
    build files are minimized, making the roll much easier
    to do.
    
    [1]: https://gn-review.googlesource.com/c/gn/+/22000
    
    Change-Id: I089b7c82bb2e5de96b46b69a54c37255e53bc480

--- src/gn/command_format.h
+++ src/gn/command_format.h
@@ -24,8 +24,23 @@ enum class TreeDumpMode {
   kJSON
 };
 
+// The default format width.
+extern const size_t kDefaultFormatWidth;
+
+bool FormatJsonToString(const std::string& input,
+                        size_t format_width,
+                        std::string* output);
+
+// Same as above, uses default format width
 bool FormatJsonToString(const std::string& input, std::string* output);
 
+bool FormatStringToString(const std::string& input,
+                          TreeDumpMode dump_tree,
+                          size_t format_width,
+                          std::string* output,
+                          std::string* dump_output);
+
+// Same as above, uses default format width.
 bool FormatStringToString(const std::string& input,
                           TreeDumpMode dump_tree,
                           std::string* output,

d020fc61a7a155ee085092857cb0a68e5c64188d
Matt Stark <msta@google.com>
2026-06-05 14:22:05 +1000

    Create starlark attr crate
    Note for reviewers - I strongly advise reading the code in the following order:
    * globals.rs - Contains the starlark attr.* functions, which creates
    * schema.rs - Contains the schema of an attribute (all the metadata about the type), which is used to create an
    * attr.rs - Contains the value of an attribute, which can be converted to
    * value.rs - Converts an attribute to a starlark value (this part is done in a follow-up CL).
    
    Bug: 528225104
    Change-Id: I14f205864735184f8a23295151f0a6596a6a6964

---
+++ src/gn/starlark/crates/attr/src/errors.rs
@@ -0,0 +1,52 @@
+// Copyright 2026 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use std::path::PathBuf;
+
+use starlark::values::UnpackValueError;
+
+/// Errors returned by target attribute validation and coercion.
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum Error {
+    #[error("Attribute `{param}` is mandatory")]
+    MandatoryAttribute { param: String },
+    #[error("Want non-empty list, got []")]
+    EmptyListDisallowed,
+    #[error("Want non-empty dict, got {{}}")]
+    EmptyDictDisallowed,
+    #[error("File \"{file:?}\" has disallowed extension, allowed extensions are: {allowed:?}")]
+    DisallowedExtension { file: PathBuf, allowed: Vec<String> },
+    #[error("Config transition not implemented: {0}")]
+    ConfigTransitionNotImplemented(String),
+
+    #[error("mandatory and default are mutually exclusive")]
+    MandatoryAndDefaultMutuallyExclusive,
+
+    #[error("Value {0} is not in allowed set")]
+    IntNotAllowed(i64),
+    #[error("Value {0:?} is not in allowed set")]
+    StringNotAllowed(String),
+
+    #[error("allow_files and allow_single_file are mutually exclusive")]
+    AllowFilesMutuallyExclusive,
+    #[error("allow_empty = False requires the attribute to be mandatory or have a non-empty default value")]
+    AllowEmptyRequiresMandatoryOrDefault,
+
+    #[error("Not a label: {0}")]
+    NotALabel(String),
+    #[error("Label `{0}` is duplicated in attribute {1}")]
+    DuplicateLabel(crate::LabelOrFile, String),
+}
+
+impl From<Error> for starlark::Error {
+    fn from(err: Error) -> Self {
+        Self::new_other(err)
+    }
+}
+
+impl UnpackValueError for Error {
+    fn into_error(this: Self) -> starlark::Error {
+        starlark::Error::new_other(this)
+    }
+}

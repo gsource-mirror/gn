1657d69a282480c2da52fe12065608d4877eb7bc
Matt Stark <msta@google.com>
2026-06-05 14:22:05 +1000

    Create starlark providers crate
    Bug: 528225104
    Change-Id: I6691d035e7f5b43b48a32debb3f4e8c76a6a6964

---
+++ src/gn/starlark/crates/providers/src/globals.rs
@@ -0,0 +1,35 @@
+use starlark::{environment::GlobalsBuilder, eval::Evaluator, values::Value};
+use starlark_derive::starlark_module;
+
+use crate::errors::Error;
+
+/// Registers the global `provider()` function.
+#[starlark_module]
+pub(crate) fn register_providers_globals(builder: &mut GlobalsBuilder) {
+    fn provider<'v>(
+        #[starlark(require = named)] fields: Value<'v>,
+        eval: &mut Evaluator<'v, '_, '_>,
+    ) -> starlark::Result<Value<'v>> {
+        let mut field_names = Vec::new();
+        for field in fields
+            .iterate(eval.heap())
+            .map_err(|_| Error::FieldsMustBeIterable)?
+        {
+            let s = field
+                .unpack_str()
+                .ok_or(Error::FieldsMustBeStrings)?
+                .to_owned();
+            if field_names.contains(&s) {
+                return Err(Error::DuplicateFieldName(s).into());
+            }
+            field_names.push(s);
+        }
+
+        let provider_type = crate::provider_type::ProviderType::new(field_names);
+        Ok(eval.heap().alloc_complex(provider_type))
+    }
+}
+
+pub fn register_providers(builder: &mut GlobalsBuilder) {
+    register_providers_globals(builder);
+}

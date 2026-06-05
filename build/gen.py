4abedfd08d315cff7eef768abfe6be54e7d50b1a
Matt Stark <msta@google.com>
2026-06-05 14:22:05 +1000

    Add `load` function to GN
    Bug: 528225104
    Change-Id: I52784a4173e6b4ee6ee675255b95ee9e6a6a6964

--- build/gen.py
+++ build/gen.py
@@ -1152,6 +1152,7 @@ def WriteGNNinja(path, platform, host, options, args_list):
         'src/gn/ffi/session_unittest.cc',
     ])
     executables['gn_unittests']['libs'].append('gn_starlark')
+    executables['gn']['libs'].append('gn_starlark')
 
   # Write the absolute path of the source root to a file in the output directory
   # so that tests can locate the source tree robustly.

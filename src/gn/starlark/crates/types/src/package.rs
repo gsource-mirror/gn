7ca4b9bbb30e79e848276d87934ade73f2998bbb
msta <msta@google.com>
2026-06-26 12:04:22 +1000

    Create starlark types crate.
    This crate contains the core bazel types that all other crates depend on.
    
    For now, this crate can only be built with cargo.
    Note: Tests require linking in a .a file from GN. Running the tests
    thus requires first building gn.
    
    Bug: 528225104
    Change-Id: I805094989f01480a3875a38c191ddb5e6a6a6964

---
+++ src/gn/starlark/crates/types/src/package.rs
@@ -0,0 +1,37 @@
+// Copyright 2026 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use crate::PackageRef;
+
+/// A Package is a String that is either of the form "//" or "//foo/bar"
+/// It differs from a SourceDir in that it never ends with a "/"
+/// Always prefer &PackageRef over Package to avoid copies.
+#[derive(Debug, Clone, Eq, PartialEq, Hash, allocative::Allocative)]
+pub struct Package(pub(crate) String);
+
+impl std::ops::Deref for Package {
+    type Target = PackageRef;
+    fn deref(&self) -> &Self::Target {
+        // Safety: already validated
+        unsafe { PackageRef::new_unchecked(&self.0) }
+    }
+}
+
+impl AsRef<PackageRef> for Package {
+    fn as_ref(&self) -> &PackageRef {
+        self
+    }
+}
+
+impl std::borrow::Borrow<PackageRef> for Package {
+    fn borrow(&self) -> &PackageRef {
+        self
+    }
+}
+
+impl std::fmt::Display for Package {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
---
+++ src/gn/starlark/crates/types/src/util.rs
@@ -0,0 +1,8 @@
+// Copyright 2026 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Like std::mem::transmute, but can only affect lifetime.
+pub unsafe fn extend_lifetime<'to, 'from, T: ?Sized>(val: &'from T) -> &'to T {
+    unsafe { std::mem::transmute(val) }
+}

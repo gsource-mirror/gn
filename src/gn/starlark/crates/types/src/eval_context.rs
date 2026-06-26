1bfa9540cd0eab730ba5a6a0b2ceb4e2cac0f62d
msta <msta@google.com>
2026-06-26 12:04:22 +1000

    Create starlark types crate.
    This crate contains the core bazel types that all other crates depend on.
    
    For now, this crate can only be built with cargo.
    Note: Tests require linking in a .a file from GN. Running the tests
    thus requires first building gn.
    
    Change-Id: I805094989f01480a3875a38c191ddb5e6a6a6964

---
+++ src/gn/starlark/crates/types/src/eval_context.rs
@@ -0,0 +1,84 @@
+// Copyright 2026 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use crate::{LabelRef, PackageRef, PathResolver, Session};
+
+/// The starlark Evaluator has an "extra" object that you can use to store
+/// whatever metadata you want, accessible to any custom functions you write
+/// in starlark.
+///
+/// This interface contains the specific features we need to access for our
+/// bazel-like API.
+pub trait EvalContext:
+    for<'v> starlark::values::ProvidesStaticType<'v, StaticType = Self>
+    + allocative::Allocative
+    + Send
+    + Sync
+    + 'static
+{
+    /// The session type associated with this context.
+    type Session: Session;
+
+    /// Returns the package currently being evaluated.
+    fn current_package(&self) -> &PackageRef;
+
+    /// Returns the path resolver to map source files to output files.
+    fn path_resolver(&self) -> &PathResolver;
+
+    /// Returns the Starlark session.
+    fn session(&self) -> &Self::Session;
+
+    /// Returns the label of the current toolchain.
+    fn current_toolchain(&self) -> LabelRef<'_>;
+
+    /// Asserts that the current evaluation context is a macro context.
+    /// Use when a function cannot be called from other contexts (eg. `static_library` cannot be called inside rule evaluation)
+    fn require_macro(&self) -> starlark::Result<()>;
+
+    /// Asserts that the current evaluation context is a bzl file context.
+    fn require_bzl(&self) -> starlark::Result<()>;
+
+    /// Asserts that the evaluator is executing a rule implementation, and returns the state of the rule implementation.
+    fn require_rule_impl(
+        &self,
+    ) -> starlark::Result<&crate::CtxState<<Self::Session as Session>::TargetRef>>;
+
+    /// Asserts that the evaluator is executing a rule implementation, and returns the mutable state of the rule implementation.
+    fn require_rule_impl_mut(
+        &mut self,
+    ) -> starlark::Result<&mut crate::CtxState<<Self::Session as Session>::TargetRef>>;
+}
+
+/// Extension trait to add the methods `.context` and `.context_mut` to the starlark Evaluator.
+pub trait EvaluatorContextExt<'v, 'a, 'e> {
+    /// Returns a reference to the evaluation context.
+    fn context<C: EvalContext>(&self) -> &C;
+
+    /// Returns a mutable reference to the evaluation context.
+    fn context_mut<C: EvalContext>(&mut self) -> &mut C;
+}
+
+impl<'v, 'a, 'e> EvaluatorContextExt<'v, 'a, 'e> for starlark::eval::Evaluator<'v, 'a, 'e> {
+    #[inline]
+    fn context<C: EvalContext>(&self) -> &C {
+        let extra = self.extra.as_ref();
+        // Safety: Extra *always* contains the evaluator context payload.
+        unsafe { extra.unwrap_unchecked() }
+            .downcast_ref::<C>()
+            .unwrap()
+    }
+
+    #[inline]
+    fn context_mut<C: EvalContext>(&mut self) -> &mut C {
+        let extra = self.extra.as_mut();
+        // Safety: Extra *always* contains the evaluator context payload.
+        let val_ref = unsafe { extra.unwrap_unchecked() }
+            .downcast_ref::<C>()
+            .unwrap();
+        // Safety: We know that context is not borrowed already, either
+        // mutably or immutably, because that would require a second
+        // borrow of the evaluator itself while it's borrowed immutably.
+        unsafe { crate::util::add_mut(val_ref) }
+    }
+}

7f6ac9d29e4cdfd4e83d0047a32feb8ff63d6706
msta <msta@google.com>
2026-06-26 12:04:22 +1000

    Create starlark types crate.
    This crate contains the core bazel types that all other crates depend on.
    
    For now, this crate can only be built with cargo.
    Note: Tests require linking in a .a file from GN. Running the tests
    thus requires first building gn.
    
    Change-Id: I805094989f01480a3875a38c191ddb5e6a6a6964

---
+++ src/gn/starlark/crates/types/src/util.rs
@@ -0,0 +1,14 @@
+// Copyright 2026 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Like std::mem::transmute, but can only affect lifetime.
+pub unsafe fn extend_lifetime<'to, 'from: 'from, T: ?Sized>(val: &'from T) -> &'to T {
+    unsafe { std::mem::transmute(val) }
+}
+
+/// Like std::mem::transmute, but limited to only adding mutability.
+#[allow(invalid_reference_casting)]
+pub unsafe fn add_mut<'to, 'from, T: ?Sized>(val: &'from T) -> &'to mut T {
+    unsafe { &mut *(val as *const T as *mut T) }
+}

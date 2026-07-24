f9ad49baf67b5c2ad0c132c6527ee08884e449af
Zgroza Klimek <zgroza@chromium.org>
2026-07-24 13:08:21 +0200

    Add support for remaining target types in gn ls --type
    Specifically, this adds the ability to filter targets by:
    - rust_library
    - rust_proc_macro
    - bundle_data
    - create_bundle
    - generated_file
    
    The is needed to efficiently list elements with the type rust_library
    in `autotest.py` script in Chromium: http://crrev.com/c/8096846
    
    Bug: 534608615
    Change-Id: I148f78484c495c995d2373a7f51114676a6a6964

--- src/gn/commands_unittest.cc
+++ src/gn/commands_unittest.cc
@@ -4,6 +4,9 @@
 
 #include <stddef.h>
 
+#include "base/command_line.h"
+#include "base/values.h"
+
 #include "gn/commands.h"
 #include "gn/label_pattern.h"
 #include "gn/target.h"
@@ -32,4 +35,55 @@ TEST(Commands, FilterOutMatch) {
 
   EXPECT_EQ(1, output.size());
   EXPECT_EQ(&target_cbar, output[0]);
+}
+
+TEST(Commands, ApplyTypeFilter) {
+  TestWithScope setup;
+  SourceDir current_dir("//");
+
+  static constexpr std::pair<std::string, Target::OutputType> cases[] = {
+      {"group", Target::GROUP},
+      {"executable", Target::EXECUTABLE},
+      {"shared_library", Target::SHARED_LIBRARY},
+      {"loadable_module", Target::LOADABLE_MODULE},
+      {"static_library", Target::STATIC_LIBRARY},
+      {"source_set", Target::SOURCE_SET},
+      {"copy", Target::COPY_FILES},
+      {"action", Target::ACTION},
+      {"generated_file", Target::GENERATED_FILE},
+      {"rust_library", Target::RUST_LIBRARY},
+      {"rust_proc_macro", Target::RUST_PROC_MACRO},
+      {"bundle_data", Target::BUNDLE_DATA},
+      {"create_bundle", Target::CREATE_BUNDLE},
+  };
+
+  std::vector<std::unique_ptr<Target>> created_targets;
+  std::vector<const Target*> all_targets;
+
+  for (const auto& test_case : cases) {
+    auto target = std::make_unique<Target>(
+        setup.settings(), Label(SourceDir("//a/"), test_case.first));
+    target->set_output_type(test_case.second);
+    all_targets.push_back(target.get());
+    created_targets.push_back(std::move(target));
+  }
+
+  for (const auto& test_case : cases) {
+    std::vector<const Target*> targets_to_filter = all_targets;
+
+    base::CommandLine cmdline(base::CommandLine::NO_PROGRAM);
+    cmdline.AppendSwitch("type", test_case.first);
+
+    commands::CommandSwitches::Init(cmdline);
+
+    base::ListValue out;
+    commands::FilterAndPrintTargets(&targets_to_filter, &out);
+
+    EXPECT_EQ(1u, targets_to_filter.size());
+    EXPECT_EQ(test_case.second, targets_to_filter[0]->output_type())
+        << "Failed for type: " << test_case.first;
+
+    commands::CommandSwitches empty_switches;
+    commands::CommandSwitches::Set(empty_switches);
+  }
 }

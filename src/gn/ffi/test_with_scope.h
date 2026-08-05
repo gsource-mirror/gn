63090bd1c25471492e7022649b386078117def1d
msta <msta@google.com>
2026-08-06 09:56:00 +1000

    Fix rust unittests when running with asan.
    There were two issues:
    * The command-line was not being freed
    * We were freeing memory with free that was alloced with new. It is
      preferred to use delete for any memory allocated with new.
    
    Bug: 528225104
    Change-Id: Iadd110060b0f690cb4f4697d04445af06a6a6964

--- src/gn/ffi/test_with_scope.h
+++ src/gn/ffi/test_with_scope.h
@@ -5,6 +5,7 @@
 #ifndef TOOLS_GN_FFI_TEST_WITH_SCOPE_H_
 #define TOOLS_GN_FFI_TEST_WITH_SCOPE_H_
 
+#include <cstdlib>
 #include <memory>
 
 #include "base/command_line.h"
@@ -16,6 +17,7 @@ inline std::unique_ptr<TestWithScope> NewTestWithScope() {
     int argc = 1;
     const char* argv[] = {"gn_rust_tests", nullptr};
     base::CommandLine::Init(argc, argv);
+    std::atexit(base::CommandLine::Reset);
   }
   // Initialize a single Scheduler instance that lives for the lifetime of the
   // test process. This ensures g_scheduler is always set and never cleared

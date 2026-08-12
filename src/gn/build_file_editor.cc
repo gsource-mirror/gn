4f444cf86fb4fe763a7a2b3a363060b9bd6a135c
msta <msta@google.com>
2026-08-12 10:05:21 +1000

    Implement `gn edit delete` and `gn edit remove attr`
    Change-Id: Ib41ef3fa55e622cecd6fea3e748305ef6a6a6964

--- src/gn/build_file_editor.cc
+++ src/gn/build_file_editor.cc
@@ -89,7 +89,7 @@ Result<std::vector<SourceFile>> ResolvePatternToFiles(
 }  // namespace
 
 bool TreeNode::is_conditional() const {
-  for (auto it = stack_.rbegin(); it != stack_.rend(); ++it) {
+  for (auto it = stack_.rbegin() + 1; it != stack_.rend(); ++it) {
     if ((*it)->AsCondition()) {
       return true;
     }
